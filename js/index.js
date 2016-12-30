ca44f30f50170c92e78d09b2d1c6271b39988229
Roberto Hernandez <hernandez@smu.edu>
2016-12-29 18:49:08 -0600

    new me

---
+++ js/index.js
@@ -0,0 +1,44 @@
+/*----------------------------------------------------*/
+/* Quote Loop
+------------------------------------------------------ */
+
+function fade($ele) {
+    $ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
+        var $next = $(this).next('.quote');
+        fade($next.length > 0 ? $next : $(this).parent().children().first());
+   });
+}
+fade($('.quoteLoop > .quote').first());
+
+
+/*----------------------------------------------------*/
+/* Navigation
+------------------------------------------------------ */
+
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 300) {
+        $('.main_nav').addClass('sticky');
+    } else {
+        $('.main_nav').removeClass('sticky');
+    }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_nav').hasClass('open-nav')) {
+        $('.main_nav').removeClass('open-nav');
+    } else {
+        $('.main_nav').addClass('open-nav');
+    }
+});
+
+$('.main_nav li a').click(function() {
+    if ($('.main_nav').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_nav').removeClass('open-nav');
+    }
+});
+
+
+TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
